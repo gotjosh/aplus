1bca9a21c29e09be0ae7073bdc001457d8225c65
Teodoro Alcantara Aquino <teodoro641@hotmail.com>
2015-06-19 22:46:52 -0400

    Added has_many relationship to topic factory

--- spec/factories/topic_factory.rb
+++ spec/factories/topic_factory.rb
@@ -2,5 +2,13 @@ FactoryGirl.define do
   factory :topic do
     title "Planet Zero"
     reviewing true
+    user
+
+    factory :topic_with_cards do
+      after(:create) do |topic|
+        create(:card, topic: topic)
+      end
+    end
   end
 end
+
--- spec/models/topic_spec.rb
+++ spec/models/topic_spec.rb
@@ -1,12 +1,10 @@
 require 'rails_helper'
-require 'spec_helper'
 
 describe Topic do
   it "Can reset all cards in a topic" do
-    topic = create(:topic)
+    topic = create(:topic_with_cards)
     Topic.reset_cards topic
     topic.reload
-    p topic.cards
     topic.cards.each do |card|
       expect(card.level).to eq(1)
     end
